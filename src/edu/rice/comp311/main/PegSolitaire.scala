8e94314ee8673c46d8373142b85c6c21e961b1f6
victor515 <victoroyyb@gmail.com>
2018-10-29 23:04:27 -0500

    finish solve, all tests passed

--- src/edu/rice/comp311/main/PegSolitaire.scala
+++ src/edu/rice/comp311/main/PegSolitaire.scala
@@ -9,33 +9,110 @@ object PegSolitaire {
     * @return One possible solution to the peg solitaire(list of moves), or None if none is found
     */
   def solve(size: Int, hole: (Int,Int)): Option[List[Move]] = {
-    /**
-      *
-      * @param board current board
-      * @param moves list of moves leading to current board state
-      * @return one possible solution to the peg solitaire problem, represented by a list of moves
-      */
-    def inner(board: Board, moves: List[Move]): Option[List[Move]] = {
-      board match {
-        case Board(_, 1, _) => Option(moves)
-        case _ => {
-          // TODO: find all possible next moves, generate new boards with them and continue to solve
-          inner()
+    val coordinates = {
+      for(i <- 0 to size - 1; j <- 0 to i)
+        yield (i, j)
+    }
+//    println(coordinates)
+
+    solveHelper(Board(size * (size + 1) / 2 - 1, Set(hole)), coordinates.toList, List())
+  }
+
+  def solveHelper(board: Board, coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
+//    println(board)
+    if(board.peg == 1){
+      Some(moves.reverse)
+    }
+    else {
+//      coordinates match {
+//        case Nil => None
+//        case x :: xs => {
+//          nextMove(board, x) match {
+//            case Nil => tryOtherCoordinates(board, xs, coordinates, moves)
+//            case move :: otherMoves => {
+//              solveHelper(reduceBoard(board, move), coordinates, move :: moves) match {
+//                case None => tryOtherMoves(otherMoves, board, coordinates, moves)
+//                case Some(m) => Some(m)
+//              }
+//            }
+//          }
+//        }
+//      }
+      runOneMove(board, coordinates, coordinates, moves)
+    }
+  }
+
+  def runOneMove(board: Board, remained: List[(Int, Int)], coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
+    remained match {
+      case Nil => None
+      case x :: xs => {
+        nextMove(board, x) match {
+          case Nil => runOneMove(board, xs, coordinates, moves)
+//          case move :: otherMoves => {
+//            solveHelper(reduceBoard(board, move), coordinates, move :: moves) match {
+//              case None => tryOtherMoves(otherMoves, board, coordinates, moves)
+//              case Some(m) => Some(m)
+//            }
+//          }
+          case nextMoves: List[Move] => {
+            tryNextMoves(nextMoves, board, coordinates, moves) match {
+              case None => runOneMove(board, xs, coordinates, moves)
+              case Some(m) => Some(m)
+            }
+          }
         }
       }
-
     }
-
-    inner(Board(size, size * (size + 1)/2, Set(hole)), List())
   }
+
+  def tryNextMoves(nextMoves: List[Move], board: Board, coordinates: List[(Int, Int)],
+                   moves: List[Move]): Option[List[Move]] = {
+    nextMoves match {
+      case Nil => None
+      case move :: others => {
+        solveHelper(reduceBoard(board, move), coordinates, move :: moves) match {
+          case None => tryNextMoves(others, board, coordinates, moves)
+          case Some(m) => Some(m)
+        }
+      }
+    }
+  }
+
 
   /**
     *
-    * @param board current board
-    * @return set of all possible moves
+    * @param board current board state
+    * @param peg the given peg position
+    * @return possible moves for the given peg
     */
-  def nextMoves(board: Board): Option[Set[Move]] = {
-    return None
+  def nextMove(board: Board, peg: (Int, Int)): List[Move] = {
+    var ans: List[Move] = Nil
+    val holes = board.holes
+
+    if(holes contains peg){
+      ans
+    }
+    else {
+      if ((holes contains(peg._1, peg._2 - 2)) && !(holes contains(peg._1, peg._2 - 1))) {
+        ans = Move(peg, (peg._1, peg._2 - 2)) :: ans
+      }
+      if ((holes contains(peg._1, peg._2 + 2)) && !(holes contains(peg._1, peg._2 + 1))) {
+        ans = Move(peg, (peg._1, peg._2 + 2)) :: ans
+      }
+      if ((holes contains(peg._1 - 2, peg._2 - 2)) && !(holes contains(peg._1 - 1, peg._2 - 1))) {
+        ans = Move(peg, (peg._1 - 2, peg._2 - 2)) :: ans
+      }
+      if ((holes contains(peg._1 - 2, peg._2)) && !(holes contains(peg._1 - 1, peg._2))) {
+        ans = Move(peg, (peg._1 - 2, peg._2)) :: ans
+      }
+      if ((holes contains(peg._1 + 2, peg._2)) && !(holes contains(peg._1 + 1, peg._2))) {
+        ans = Move(peg, (peg._1 + 2, peg._2)) :: ans
+      }
+      if ((holes contains(peg._1 + 2, peg._2 + 2)) && !(holes contains(peg._1 + 1, peg._2 + 1))) {
+        ans = Move(peg, (peg._1 + 2, peg._2 + 2)) :: ans
+      }
+      ans
+    }
   }
 
   /**
@@ -44,7 +121,13 @@ object PegSolitaire {
     * @param move next move
     * @return next board state
     */
-  def reduceBoard(board: Board, move: Move): Board = ???
+  def reduceBoard(board: Board, move: Move): Board = {
+    val toDelete = move.to
+    val toAdd1 = move.from
+    val toAdd2 = ((move.from._1 + move.to._1)/2, (move.from._2 + move.to._2)/2)
+    val holes = (board.holes + toAdd1 + toAdd2 - toDelete)
+    Board(board.peg - 1, holes)
+  }
 
 
 }
@@ -56,8 +139,9 @@ case class Move(from: (Int, Int), to: (Int, Int)) extends Removal {
 }
 
 /**
-  * @param size the size of board
-  * @param pegs number of pegs remained in the board
+  * @param peg list of coordinates in the board
   * @param holes positions of holes in the board
   */
-case class Board(size: Int, pegs: Int, holes: Set[(Int, Int)])
+case class Board(peg: Int, holes: Set[(Int, Int)]){
+  override def toString() = "Pegs: " + peg + "Holes: " + holes.toString()
+}
--- src/edu/rice/comp311/test/PegSolitaireTest.scala
+++ src/edu/rice/comp311/test/PegSolitaireTest.scala
@@ -1,9 +1,49 @@
 package edu.rice.comp311.test;
+import edu.rice.comp311.main._
+import edu.rice.comp311.main.PegSolitaire._;
 
-import junit.framework.TestCase;
-import junit.framework.Assert._;
+import junit.framework.TestCase
+import org.junit.Assert._
 
 
 case class PegSolitaireTest(name: String) extends TestCase(name){
 
+
+  /**
+    *      x
+    *     x x   -> find next move for (0,0): Move((0,0),(2,0))
+    *    o x x
+    */
+  def testNextMove1: Unit = {
+    val board = Board(6, Set((2,0)))
+    val peg = (0,0)
+    assertEquals("Next move for 3*3 board is correct" +
+      "hole: (2,0), peg: (0,0)", List(Move((0,0), (2, 0))), nextMove(board, peg))
+  }
+
+  def testNextMove2: Unit = {
+    val board = Board(6, Set((2,2)))
+    val peg = (0,0)
+    assertEquals("Next move for 3*3 board is correct" +
+      "hole: (2,2), peg: (0,0)", List(Move((0,0), (2, 2))), nextMove(board, peg))
+  }
+
+  def testReduceBoard: Unit = {
+    val board = Board(6, Set((2,2)))
+    val move = Move((0,0), (2,2))
+    assertEquals("Next state of 3*3 board is correct: " +
+      "hole: (2,2), move: (0,0) -> (2,2)", Board(5, Set((0,0),(1,1))), reduceBoard(board, move))
+  }
+
+  def testSolve1: Unit = {
+    assertEquals("Board of size 3 is not solvable", true, solve(3, (2,0)).isEmpty)
+  }
+
+  def testSolve2: Unit = {
+    assertEquals("Board of size 5 is solvable", true, solve(5, (2,0)).nonEmpty)
+  }
+
+  def testSolve3: Unit = {
+    assertEquals("Board of size 7 with one hole at (1,0) is solvable", true, solve(7, (1,0)).nonEmpty)
+  }
 }

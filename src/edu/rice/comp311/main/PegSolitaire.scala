442917bd48b7ae735ff5e7f478af853e1845ff16
victor515 <victoroyyb@gmail.com>
2018-11-01 10:15:48 -0500

    finish all code, bigstepsolve not tested

--- src/edu/rice/comp311/main/PegSolitaire.scala
+++ src/edu/rice/comp311/main/PegSolitaire.scala
@@ -1,5 +1,7 @@
 package edu.rice.comp311.main
 
+import scala.collection.immutable.Queue
+
 
 object PegSolitaire {
   /**
@@ -9,51 +11,34 @@ object PegSolitaire {
     * @return One possible solution to the peg solitaire(list of moves), or None if none is found
     */
   def solve(size: Int, hole: (Int,Int)): Option[List[Move]] = {
+    val coordinates = generateCoordinates(size)
+
+    solveHelper(Board(size * (size + 1) / 2 - 1, Set(hole)), coordinates, List())
+  }
+
+  def generateCoordinates(size: Int): List[(Int, Int)] ={
     val coordinates = {
       for(i <- 0 to size - 1; j <- 0 to i)
         yield (i, j)
     }
-//    println(coordinates)
-
-    solveHelper(Board(size * (size + 1) / 2 - 1, Set(hole)), coordinates.toList, List())
+    coordinates.toList
   }
 
-  def solveHelper(board: Board, coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
-//    println(board)
+  private def solveHelper(board: Board, coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
     if(board.peg == 1){
       Some(moves.reverse)
     }
     else {
-//      coordinates match {
-//        case Nil => None
-//        case x :: xs => {
-//          nextMove(board, x) match {
-//            case Nil => tryOtherCoordinates(board, xs, coordinates, moves)
-//            case move :: otherMoves => {
-//              solveHelper(reduceBoard(board, move), coordinates, move :: moves) match {
-//                case None => tryOtherMoves(otherMoves, board, coordinates, moves)
-//                case Some(m) => Some(m)
-//              }
-//            }
-//          }
-//        }
-//      }
       runOneMove(board, coordinates, coordinates, moves)
     }
   }
 
-  def runOneMove(board: Board, remained: List[(Int, Int)], coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
+  private def runOneMove(board: Board, remained: List[(Int, Int)], coordinates: List[(Int, Int)], moves: List[Move]): Option[List[Move]] = {
     remained match {
       case Nil => None
       case x :: xs => {
         nextMove(board, x) match {
           case Nil => runOneMove(board, xs, coordinates, moves)
-//          case move :: otherMoves => {
-//            solveHelper(reduceBoard(board, move), coordinates, move :: moves) match {
-//              case None => tryOtherMoves(otherMoves, board, coordinates, moves)
-//              case Some(m) => Some(m)
-//            }
-//          }
           case nextMoves: List[Move] => {
             tryNextMoves(nextMoves, board, coordinates, moves) match {
               case None => runOneMove(board, xs, coordinates, moves)
@@ -65,7 +50,7 @@ object PegSolitaire {
     }
   }
 
-  def tryNextMoves(nextMoves: List[Move], board: Board, coordinates: List[(Int, Int)],
+  private def tryNextMoves(nextMoves: List[Move], board: Board, coordinates: List[(Int, Int)],
                    moves: List[Move]): Option[List[Move]] = {
     nextMoves match {
       case Nil => None
@@ -129,6 +114,153 @@ object PegSolitaire {
     Board(board.peg - 1, holes)
   }
 
+  def bigStepSolve(size: Int, hole: (Int, Int)): Option[List[Removal]] = {
+    val coordinates = generateCoordinates(size)
+
+    bigStepSolveHelper(Queue(Board(size * (size + 1) / 2 - 1, Set(hole))), coordinates, Queue(List()))
+  }
+
+  def bigStepSolveHelper(boards: Queue[Board], coordinates: List[(Int, Int)],
+                         removals: Queue[List[Removal]]): Option[List[Removal]] = {
+    // TODO:
+    // 1. Dequeue a board and its move list
+    // 2. Find all possible next moves
+    // 3. generate next boards, add to queue
+    // 4. If queue is not empty, repeat
+    // Do I need to set to record visited boards?
+    val (curBoard,boardqTemp) = boards.dequeue
+    val (removalList, removalqTemp) = removals.dequeue
+
+    if(curBoard.peg == 1){
+      Some(removalList.reverse)
+    }else{
+      val (nextBoards, nextRemovals) = getNextBoard(curBoard, removalList, coordinates)
+      val boardq = boardqTemp.enqueue(nextBoards)
+      val removalq = removalqTemp.enqueue(nextRemovals)
+      if(boardq.nonEmpty){
+        bigStepSolveHelper(boardq, coordinates, removalq) // BFS
+      }else{
+        None
+      }
+    }
+
+  }
+
+  /**
+    *
+    * @param board current board
+    * @param removalList the removal list leading to current board state
+    * @param coordinates coordinates of board
+    * @return list of all possible next boards, and list of all corresponding removal lists
+    */
+  def getNextBoard(board: Board, removalList: List[Removal],
+                   coordinates: List[(Int, Int)]): (List[Board], List[List[Removal]]) = {
+    coordinates match {
+      case Nil => (Nil, Nil)
+      case coord :: others => {
+        nextRemoval(board, coord, coordinates) match {
+          case Nil => getNextBoard(board, removalList, others)
+          case removals => {
+            val (boardList1, removalList1) = runOneRemoval(board, removalList, removals)
+            val (boardList2, removalList2) = getNextBoard(board, removalList, others)
+            (boardList1 ++ boardList2, removalList1 ++ removalList2)
+          }
+        }
+      }
+    }
+  }
+
+  def runOneRemoval(board: Board, rmList: List[Removal],
+                    removals: List[Removal]): (List[Board], List[List[Removal]]) = {
+    removals match {
+      case Nil => (Nil, Nil)
+      case removal :: others => {
+        val (boardList: List[Board], removalList: List[List[Removal]]) = runOneRemoval(board, rmList, others)
+        (reduceBoardWithRemoval(board, removal) :: boardList, (removal :: rmList) :: removalList)
+      }
+    }
+  }
+
+  def nextRemoval(board: Board, peg: (Int, Int), coordinates: List[(Int, Int)]): List[Removal] = {
+    val holes = board.holes
+    var ans: List[Removal] = Nil
+    if(holes contains peg){
+      ans
+    }
+    ans = nextMove(board, peg)
+    val (x, y) = peg
+
+    var pegsRemoval :List[(Int, Int)] = Nil
+    if(!(holes contains (x - 2, y - 2)) && !(holes contains (x - 1, y - 1))
+      && (coordinates contains (x - 2, y - 2)) && (coordinates contains (x - 1, y - 2))){
+      pegsRemoval = (x - 2, y - 2) :: pegsRemoval
+    }
+    if(!(holes contains (x - 2, y)) && !(holes contains (x - 1, y))
+      && (coordinates contains (x - 2, y)) && (coordinates contains (x - 1, y))){
+      pegsRemoval = (x - 2, y) :: pegsRemoval
+    }
+    if(!(holes contains (x, y - 2)) && !(holes contains (x, y - 1))
+      && (coordinates contains (x, y - 2)) && (coordinates contains (x, y - 1))){
+      pegsRemoval = (x, y - 2) :: pegsRemoval
+    }
+    if(!(holes contains (x, y + 2)) && !(holes contains (x, y + 1))
+      && (coordinates contains (x, y + 2)) && (coordinates contains (x, y + 1))){
+      pegsRemoval = (x, y + 2) :: pegsRemoval
+    }
+    if(!(holes contains (x + 2, y)) && !(holes contains (x + 1, y))
+      && (coordinates contains (x + 2, y)) && (coordinates contains (x + 1, y))){
+      pegsRemoval = (x + 2, y) :: pegsRemoval
+    }
+    if(!(holes contains (x + 2, y + 2)) && !(holes contains (x + 1, y + 1))
+      && (coordinates contains (x + 2, y + 2)) && (coordinates contains (x + 1, y + 1))){
+      pegsRemoval = (x + 2, y + 2) :: pegsRemoval
+    }
+
+    var hasAdjacentHoles = false
+
+    if((holes contains (x - 1, y - 1)) && !(holes contains (x + 1, y + 1))
+      && (coordinates contains (x + 1, y + 1))){
+      hasAdjacentHoles = true
+    }
+    if((holes contains (x + 1, y + 1)) && !(holes contains (x - 1, y - 1))
+      && (coordinates contains (x - 1, y - 1))){
+      hasAdjacentHoles = true
+    }
+    if((holes contains (x - 1, y)) && !(holes contains (x + 1, y))
+      && (coordinates contains (x + 1, y))){
+      hasAdjacentHoles = true
+    }
+    if((holes contains (x + 1, y)) && !(holes contains (x - 1, y))
+      && (coordinates contains (x - 1, y))){
+      hasAdjacentHoles = true
+    }
+    if((holes contains (x, y - 1)) && !(holes contains (x , y + 1))
+      && (coordinates contains (x, y + 1))){
+      hasAdjacentHoles = true
+    }
+    if((holes contains (x, y + 1)) && !(holes contains (x, y - 1))
+      && (coordinates contains (x, y - 1))){
+      hasAdjacentHoles = true
+    }
+
+    if(hasAdjacentHoles){
+      for(pegRemoval <- pegsRemoval){
+        ans = SegmentRemoval(peg, ((peg._1 + pegRemoval._1)/2, (peg._2 + pegRemoval._2)/2), pegRemoval) :: ans
+      }
+    }
+
+    ans
+  }
+
+  def reduceBoardWithRemoval(board: Board, removal: Removal): Board = {
+    removal match {
+      case Move(from, to) => reduceBoard(board, Move(from, to))
+      case SegmentRemoval(peg1, peg2, peg3) => {
+        val holes = board.holes + peg1 + peg2 + peg3
+        Board(board.peg - 3, holes)
+      }
+    }
+  }
 
 }
 
@@ -138,8 +270,12 @@ case class Move(from: (Int, Int), to: (Int, Int)) extends Removal {
   override def toString() = from + "->" + to
 }
 
+case class SegmentRemoval(peg1: (Int, Int), peg2: (Int, Int), peg3: (Int, Int))
+  extends Removal
+
+
 /**
-  * @param peg list of coordinates in the board
+  * @param peg number of pegs in the board
   * @param holes positions of holes in the board
   */
 case class Board(peg: Int, holes: Set[(Int, Int)]){
--- src/edu/rice/comp311/test/PegSolitaireTest.scala
+++ src/edu/rice/comp311/test/PegSolitaireTest.scala
@@ -15,14 +15,14 @@ case class PegSolitaireTest(name: String) extends TestCase(name){
     *    o x x
     */
   def testNextMove1: Unit = {
-    val board = Board(6, Set((2,0)))
+    val board = Board(5, Set((2,0)))
     val peg = (0,0)
     assertEquals("Next move for 3*3 board is correct" +
       "hole: (2,0), peg: (0,0)", List(Move((0,0), (2, 0))), nextMove(board, peg))
   }
 
   def testNextMove2: Unit = {
-    val board = Board(6, Set((2,2)))
+    val board = Board(5, Set((2,2)))
     val peg = (0,0)
     assertEquals("Next move for 3*3 board is correct" +
       "hole: (2,2), peg: (0,0)", List(Move((0,0), (2, 2))), nextMove(board, peg))
@@ -45,5 +45,45 @@ case class PegSolitaireTest(name: String) extends TestCase(name){
 
   def testSolve3: Unit = {
     assertEquals("Board of size 7 with one hole at (1,0) is solvable", true, solve(7, (1,0)).nonEmpty)
+  }
+
+
+  def testNextRemoval1: Unit = {
+    val board = Board(9, Set((1, 1)))
+    val coordinates = generateCoordinates(4)
+    val segRm = SegmentRemoval((2,2),(2,1),(2,0))
+    assertTrue("Next Removal of 4*4 board contains SegmentRemoval((2,2),(2,1),(2,0)), " +
+      "peg: (2,2), hole: (1,1)", nextRemoval(board, (2,2), coordinates) contains segRm)
+
+    val move = Move((3,3), (1,1))
+    assertTrue("Next Removal of 4*4 board contains Move((3,3),(1,1)), " +
+      "peg: (3,3), hole: (1,1)", nextRemoval(board, (3,3), coordinates) contains move)
+  }
+
+  def testNextRemoval2: Unit = {
+    val board = Board(9, Set((3, 3)))
+    val coordinates = generateCoordinates(4)
+    val segRm = SegmentRemoval((2,2),(2,1),(2,0))
+    assertTrue("Next Removal of 4*4 board contains SegmentRemoval((2,2),(2,1),(2,0)), " +
+      "peg: (2,2), hole: (3,3)", nextRemoval(board, (2,2), coordinates) contains segRm)
+
+    val move = Move((1,1), (3,3))
+    assertTrue("Next Removal of 4*4 board contains Move((1,1),(3,3)), " +
+      "peg: (1,1), hole: (3,3)", nextRemoval(board, (1,1), coordinates) contains move)
+  }
+
+  def testReduceBoardWithRemoval: Unit = {
+    val board = Board(9, Set((3, 3)))
+    val segRm = SegmentRemoval((2,2),(2,1),(2,0))
+    assertEquals("Next state of 4*4 board is correct: hole: (3,3), segmentRemoval: ((2,2),(2,1),(2,0))",
+      Board(6, Set((3,3),(2,2),(2,1),(2,0))), reduceBoardWithRemoval(board, segRm))
+
+    val move = Move((1,1), (3,3))
+    assertEquals("Next state of 4*4 board is correct: hole: (3,3), move: ((1,1),(3,3))",
+      Board(8, Set((1,1),(2,2))), reduceBoardWithRemoval(board, move))
+  }
+
+  def testBigStepSolve: Unit = {
+    assertEquals("Board of size 3 is not solvable", true, solve(3, (2,0)).isEmpty)
   }
 }
